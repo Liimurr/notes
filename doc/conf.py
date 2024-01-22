859d07831046fe1931e6b3b5dc76b583f62f40e5
Liam Murray <rodionskies@gmail.com>
2024-01-21 23:46:47 -0500

    use sphinx tabs

--- doc/conf.py
+++ doc/conf.py
@@ -15,7 +15,11 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = [
+  'myst_parser',
+  'sphinxawesome_theme.highlighting',
+  'sphinx_tabs.tabs'
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -25,5 +29,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinxawesome_theme"
-extensions += ["sphinxawesome_theme.highlighting"]
 html_static_path = ['_static']

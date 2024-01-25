9914a122dadd788d7255e619f89ff3ec6954c460
Liam Murray <rodionskies@gmail.com>
2024-01-24 21:28:53 -0500

    attempt to hide sidebar

--- doc/conf.py
+++ doc/conf.py
@@ -32,4 +32,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "pydata_sphinx_theme"
-html_static_path = ['_static']
+html_display_options = {
+  'display_version': False,
+  'collapse_navigation': True
+}
